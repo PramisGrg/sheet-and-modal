8a130ee75955ba3376a2ce3faa4bfcef0cca7b0d
PramisGrg <gurungpramis361@gmail.com>
2025-04-21 00:06:39 +0545

    hovered animation card

--- src/components/cards/card-item.tsx
+++ src/components/cards/card-item.tsx
@@ -4,7 +4,7 @@ const CardItem = () => {
     <div className="grid md:grid-cols-4 grid-cols-1 gap-10 max-w-6xl mx-auto py-20">
       {productData.map((product) => (
         <div
-          className="border w-56 h-64 border-gray-200 rounded-xl"
+          className="relative border group w-56 h-64 border-gray-200 rounded-xl"
           key={product.id}
         >
           <img
@@ -13,8 +13,10 @@ const CardItem = () => {
           />
 
           <div>{product.title}</div>
-          <div>
-            <button>Cart</button>
+          <div className="absolute inset-0 flex items-center justify-center bg-black/40 opacity-0 pointer-events-none group-hover:pointer-events-auto group-hover:opacity-100 transition duration-300">
+            <button className="py-2 px-6 bg-red-600 text-white cursor-pointer rounded-md transform translate-y-6 group-hover:translate-y-0 transition duration-300">
+              Cart
+            </button>
           </div>
         </div>
       ))}
